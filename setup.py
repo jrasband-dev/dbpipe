8d72fe6138783abc853ca5be015f766f0dc4eea0
Jayden Rasband <jayden.rasband@gmail.com>
2024-03-05 19:59:23 -0700

    Bumped Version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="dbpipe",
-    version="0.1.7",
+    version="0.2.0",
     description="Lightweight DataPipeline Documentation",
     long_description=README,
     long_description_content_type="text/markdown",
